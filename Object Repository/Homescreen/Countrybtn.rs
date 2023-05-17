e18c6fdc433e99bc70a73f8607f0db2bdbe05db1
ellyseatell <128705911+ellyseatell@users.noreply.github.com>
2023-05-17 10:37:19 +0300

    Add files via upload

---
+++ Object Repository/Homescreen/Countrybtn.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Countrybtn</name>
+   <tag></tag>
+   <elementGuidId>9f94c788-ecd9-429f-bf77-6747aba683a4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='country-btn']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
