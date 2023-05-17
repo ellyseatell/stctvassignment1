4c780e3a8d1af6729a60db745b7a1faae38931bd
ellyseatell <128705911+ellyseatell@users.noreply.github.com>
2023-05-17 14:23:05 +0300

    Add files via upload

---
+++ Test Suites/E2Eimagecomparison.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>E2Eimagecomparison</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2fba8078-d0fc-41c5-9c52-4504d0b9c470</testSuiteGuid>
+   <testCaseLink>
+      <guid>2e49e514-1f6a-4943-a238-784c7cfb09f1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BrowseWeb</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7fb5edcf-aa3d-4e81-994d-ca0d5df83739</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Validate Packages price and currnecy using image comparison</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
